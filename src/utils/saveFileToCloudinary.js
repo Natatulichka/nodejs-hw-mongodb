96e721e38a1a4823da360fb5ce9818f9cfc49fd9
Natalia Huliaieva <155992841+Natatulichka@users.noreply.github.com>
2024-11-07 01:36:05 +0100

    upload

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -12,7 +12,7 @@ cloudinary.v2.config({
 
 export const saveFileToCloudinary = async (file) => {
   const response = await cloudinary.v2.uploader.upload(file.path);
-  console.log('Cloudinary upload response:', response);
+
   await fs.unlink(file.path);
   return response.secure_url;
 };
