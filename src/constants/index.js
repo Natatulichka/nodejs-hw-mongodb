379d49e0f4e0dcc3d922fd1c1379e20ead2be378
Natalia Huliaieva <155992841+Natatulichka@users.noreply.github.com>
2024-11-17 17:00:08 +0100

    access

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,8 +4,7 @@ export const SORT_ORDER = {
   DESC: 'desc',
 };
 
-// export const ACCESS_TOKEN_TTL = 15 * 60 * 1000; // 15 minutes in milliseconds;
-export const ACCESS_TOKEN_TTL = 24 * 60 * 60 * 1000; // 1 day in milliseconds;
+export const ACCESS_TOKEN_TTL = 15 * 60 * 1000; // 15 minutes in milliseconds;
 export const REFRESH_TOKEN_TTL = 24 * 60 * 60 * 1000; // 1 day in milliseconds;
 
 export const SMTP = {
